7a7cd2f5d64eeb158a7d5f8017cf89eae1c8c421
targalis <targalisgroup@gmail.com>
2017-04-06 18:05:33 +0200

    Update cli-config.php

--- cli-config.php
+++ cli-config.php
@@ -6,3 +6,67 @@ require_once __DIR__ . "/vendor/autoload.php";
 $entityManager = \Model\Doctrine::getInstance()->getEntityManager();
 
 return ConsoleRunner::createHelperSet($entityManager);
+
+class Registry
+{ 
+  private $services = array();  
+  private static $instance;  
+  private function __Construct() {}  
+  public function get($key) {  
+    return $this->services[$key]; 
+  }  
+  public function add($key, $callBack) 
+  {  
+    array_push($this->services, $key);    
+    $this->services[$key] = $callBack; 
+  }  
+  
+  public function remove() 
+  {  
+    unset($this->services[$key]); 
+  }  
+  
+  public static function getInstance()    
+  {        
+    if (is_null(self::$instance)) {            
+      self::$instance = new Registry();        
+    }        
+    
+    return self::$instance;    
+  }
+}
+
+class A
+{ 
+  private static $instance;  
+  private function __Construct() {}  
+  public function getRegistry() 
+  {  
+    return Registry::getInstance(); 
+  }     
+  
+  public static function getInstance()    
+  {        
+    if (is_null(self::$instance)) {           
+      self::$instance = new A();        
+    }        
+    
+    return self::$instance;    
+  }
+}
+
+class B
+{ 
+  public function show($x, $y) 
+  {  
+    echo $x + $y; 
+  }
+}
+
+$app = A::getInstance();
+
+$app->getRegistry()->add('message', function($name) { 
+  echo $name;
+});
+
+$rB = $app->getRegistry()->get('message')('fff');
